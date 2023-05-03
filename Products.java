b74ab567149553ec5428d8b8a6c8f7216ba922bf
Rbanjali99 <rbanjali7004@gmail.com>
2023-05-03 10:42:47 +0530

    2nd

--- Products.java
+++ Products.java
@@ -1,5 +1,34 @@
 package ProductPackage;
 
 public class Products {
+    
+    private String productName;
+    private float price;
 
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+
+	public Product(String productName, float price, String description, String image) {
+		super();
+		this.productName = productName;
+		this.price = price;
+		
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+      
 }
